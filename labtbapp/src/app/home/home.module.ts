5f11ef737932ee2c954b48f36923f256cbd4b3f4
rafaelpllopes <rafael.pllopes@gmail.com>
2018-09-28 07:07:13 -0300

    Corrigindo problemas na rota

--- labtbapp/src/app/home/home.module.ts
+++ labtbapp/src/app/home/home.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
 
 import { HomeComponent } from './home.component';
 import { HomeRoutingModule } from './home.routing.module';
@@ -9,7 +8,6 @@ import { SigninComponent } from './signin/signin.component';
 @NgModule({
   imports: [
     CommonModule,
-    RouterModule,
     HomeRoutingModule
   ],
   declarations: [
