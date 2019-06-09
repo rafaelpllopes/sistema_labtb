a8b86120a109f0895b208e3ce6ac46c1fba29722
rafaelpllopes <rafael.pllopes@gmail.com>
2019-06-09 09:38:53 -0300

    Atualizado o formato de hora na lista de laudos e detalhes, setado o dia atual para laudos-periodo

--- labtbapp/src/app/relatorios/laudos-periodo/laudos-periodo.component.ts
+++ labtbapp/src/app/relatorios/laudos-periodo/laudos-periodo.component.ts
@@ -4,6 +4,7 @@ import { Observable } from 'rxjs';
 import { RelatoriosService } from '../relatorios.service';
 import { UteisService } from 'src/app/shared/services/uteis.service';
 
+import zfill from 'zfill';
 @Component({
   selector: 'app-laudos-periodo',
   templateUrl: './laudos-periodo.component.html',
@@ -22,15 +23,24 @@ export class LaudosPeriodoComponent implements OnInit {
   ) { }
 
   ngOnInit() {
+
     this.laudosPeriodo = this.build.group({
       dataInicial: ['', Validators.required],
       dataFinal: ['', Validators.required]
     });
+
+    let meses = this.uteis.getMeses();
+    const [currentMonth] = meses.filter(mes => parseInt(mes.numero) == new Date().getMonth() + 1);
+
+    let data = new Date();
+    this.laudosPeriodo.get('dataInicial').setValue(`${data.getFullYear()}-${currentMonth.numero}-${zfill(data.getDate(), 2)}`);
+    this.laudosPeriodo.get('dataFinal').setValue(`${data.getFullYear()}-${currentMonth.numero}-${zfill(data.getDate(), 2)}`);
   }
 
   buscar() {
     const dataInicial = this.laudosPeriodo.get('dataInicial').value;
     const dataFinal = this.laudosPeriodo.get('dataFinal').value;
+    console.log(dataFinal);
     this.laudos$ = this.service
       .laudosPorPeriodo(dataInicial, dataFinal);
     this.laudos$.subscribe(laudos => this.total = laudos.length);
@@ -39,5 +49,4 @@ export class LaudosPeriodoComponent implements OnInit {
   print() {
     window.print();
   }
-
 }
