9d3aa377a41e976902c2fdb4de7486ae2e40f21c
Divisão de Tecnologia da Informação e Comunicação <rafael.pllopes@gmail.com>
2019-10-15 14:27:22 -0300

    Atualizando rota usuarios

--- api/controllers/usuarios-controller.js
+++ api/controllers/usuarios-controller.js
@@ -1,10 +1,15 @@
 const db = require('../config/database')
 const jwt = require('jsonwebtoken')
-const usuariosDao = require('../infra/usuarios-dao')
+const UsuariosDao = require('../infra/usuarios-dao')
 const wrapAsync = require('../config/async-wrap')
 const { validationResult } = require('express-validator/check')
 
 class UsuarioController {
+
+    constructor() {
+        this.usuarioDao = new UsuariosDao(db)
+    }
+
     static rotas() {
         return {
             login: '/usuario/login',
@@ -12,156 +17,149 @@ class UsuarioController {
             usuarios: '/usuarios',
             filter: '/usuario/filter',
             existe: '/usuario/existe'
-        };
+        }
     }
 
     login() {
         return wrapAsync(async (req, res) => {
 
-            const erros = validationResult(req);
+            const erros = validationResult(req)
 
             if (!erros.isEmpty()) {
-                console.log(`Erros: ${erros.array()}`);
-                res.status(412).json({ msg: 'Usuário ou senha invalido' });
-                return;
+                console.log(`Erros: ${erros.array()}`)
+                res.status(412).json({ msg: 'Usuário ou senha invalido' })
+                return
             }
+            const { user_name, user_password } = req.body
 
-            const user = req.body.user_name;
-            const password = req.body.user_password;
-
-            const usuario = await new UsuariosDao(db).findUsuarioAndPassword(user, password);
+            const usuario = await this.usuarioDao.findUsuarioAndPassword(user_name, user_password);
 
             if (usuario) {
-                const token = jsonwebtoken.sign(usuario, req.app.get('secret'), {
+                const token = jwt.sign(usuario, req.app.get('secret'), {
                     expiresIn: 86400
-                });
-                res.set('x-access-token', token);
-                res.status(200).json(usuario);
+                })
+                res.set('x-access-token', token)
+                res.status(200).json(usuario)
             } else {
-                res.status(401).json({ msg: `Falha na autenticação para o usuario ${user}` })
+                res.status(401).json({ msg: 'Usuário ou senha invalido' })
             }
-        });
+
+        })
     }
 
     adicionar() {
         return wrapAsync(async (req, res) => {
-            const erros = validationResult(req);
+            const erros = validationResult(req)
 
             if (!erros.isEmpty()) {
-                res.status(412).json({ Erros: erros.array() });
-                return;
+                res.status(412).json({ Erros: erros.array() })
+                return
             }
 
-            const user = req.body.usuario.user_name;
-            const nome = req.body.usuario.user_full_name;
-            const password = req.body.usuario.user_password;
+            const { user_name, user_full_name, user_password } = req.body.usuario
 
-            const existe = await new UsuariosDao(db).existe(user);
+            const existe = await this.usuarioDao.existe(user_name)
 
             if (!existe) {
-                await new UsuariosDao(db).addUsuario(user, nome, password);
-                res.status(201).json({ msg: "Usuario cadastrado com sucesso" });
+                await this.usuarioDao.addUsuario(user_name, user_full_name, user_password)
+                res.status(201).json({ msg: "Usuário cadastrado com sucesso" })
             } else {
-                res.status(409).json({ msg: "Usuario já cadastrado" });
+                res.status(409).json({ msg: "Usuário já cadastrado" })
             }
 
-        });
+        })
     }
 
     listar() {
         return wrapAsync(async (req, res) => {
-            const page = req.query.page | 1;
-            const users = await new UsuariosDao(db).getUser(page);
+            const page = req.query.page | 1
+            const users = await this.usuarioDao.getUser(page)
             res.status(200).json(users)
-        });
+        })
     }
 
     listarPorId() {
         return wrapAsync(async (req, res) => {
-            const erros = validationResult(req);
+            const erros = validationResult(req)
 
             if (!erros.isEmpty()) {
-                res.status(412).json({ Erros: erros.array() });
-                return;
+                res.status(412).json({ Erros: erros.array() })
+                return
             }
 
-            const id = req.params.id;
-            const resultado = await new UsuariosDao(db).findUserById(id);
-            res.status(200).json(resultado);
+            const { id } = req.params;
+            const resultado = await this.usuarioDao.findUserById(id)
+            res.status(200).json(resultado)
         });
     }
 
     atualizar() {
         return wrapAsync(async (req, res) => {
-            const erros = validationResult(req);
+            const erros = validationResult(req)
 
             if (!erros.isEmpty()) {
-                res.status(412).json({ Erros: erros.array() });
-                return;
+                res.status(412).json({ Erros: erros.array() })
+                return
             }
 
-            const id = req.params.id;
-            const password = req.body.usuario.user_password;
-            const nome = req.body.usuario.user_full_name;
+            const { id } = req.params;
+            const { user_password, user_full_name } = req.body.usuario
 
-            await new UsuariosDao(db).updateUser(id, password, nome);
-            res.status(202).json({ msg: "Usuario editado com sucesso" });
-        });
+            await this.usuarioDao.updateUser(id, user_password, user_full_name)
+            res.status(202).json({ msg: "Usuário editado com sucesso" })
+        })
     }
 
     deletar() {
         return wrapAsync(async (req, res) => {
-            const erros = validationResult(req);
+            const erros = validationResult(req)
 
             if (!erros.isEmpty()) {
-                res.status(412).json({ Erros: erros.array() });
-                return;
+                res.status(412).json({ Erros: erros.array() })
+                return
             }
 
-            const id = req.params.id;
+            const { id } = req.params
 
-            const usuario = await new UsuariosDao(db).findUserById(id);
+            const usuario = await this.usuarioDao.findUserById(id)
 
             if (usuario) {
                 if (usuario.user_name !== 'admin') {
-                    await new UsuariosDao(db).deleteUserById(id)
-                    res.status(202).json({ msg: "Usuário deletado com sucesso" });
+                    await this.usuarioDao.deleteUserById(id)
+                    res.status(202).json({ msg: "Usuário deletado com sucesso" })
                 } else {
-                    res.status(403).json({ msg: "Administrador não pode ser removido" });
+                    res.status(403).json({ msg: "Administrador não pode ser removido" })
                 }
             } else {
-                res.sendStatus(412);
+                res.sendStatus(412)
             }
-
-        });
+        })
     }
 
     getByFilter() {
         return wrapAsync(async (req, res) => {
-            const user = req.query.usuario;
-            const nome = req.query.nome;
+            const { usuario, nome } = req.query
 
-            if (user || nome) {
-                const usuarios = await new UsuariosDao(db).findUserByFilter(nome, user);
-                res.status(200).json(usuarios);
+            if (usuario || nome) {
+                const usuarios = await this.usuarioDao.findUserByFilter(nome, usuario)
+                res.status(200).json(usuarios)
             } else {
-                res.status(412).json({ msg: "Não foi possivel buscar os usuarios" });
+                res.status(412).json({ msg: "Não foi possivel buscar os usuários" })
             }
-        });
-
+        })
     }
 
     getUserExiste() {
         return wrapAsync(async (req, res) => {
-            const user = req.body.user_name;
-            if (user) {
-                const usuario = await new usuariosDao(db).existe(user);
-                res.status(200).json(usuario);
+            const { user_name } = req.body
+            if (user_name) {
+                const usuario = await this.usuarioDao.existe(user_name)
+                res.status(200).json(usuario)
             } else {
-                res.status(412).json({ msg: "Nome do usuario não pode ser vazio" });
+                res.status(412).json({ msg: "Nome do usuário não pode ser vazio" })
             }
-        });
+        })
     }
 }
 
-module.exports = UsuarioController;
+module.exports = UsuarioController
--- api/models/usuario.js
+++ api/models/usuario.js
@@ -1,4 +1,4 @@
-const { check } = require('express-validator/check');
+const { check } = require('express-validator/check')
 
 const UsuarioValidacoes = {};
 
@@ -8,7 +8,7 @@ UsuarioValidacoes.login = () => [
     check('user_password').exists().withMessage('Senha é obrigatoria'),
     check('user_password').matches('[A-Za-z0-9!@#$%&*()-+?]*').withMessage('A senha deve estar no padrão'),
     check('user_password').isLength({ min: 8 }).withMessage('Senha deve possuir o mínimo de 8 caracteres!')
-];
+]
 
 UsuarioValidacoes.atualizar = () => [
     check('id').exists().withMessage('Id é obrigatoria'),
@@ -17,7 +17,7 @@ UsuarioValidacoes.atualizar = () => [
     check('usuario.user_full_name').isLength({ min: 3 }).withMessage('O nome precisa ter no mínimo 3 caracteres!'),
     check('usuario.user_password').matches('[A-Za-z0-9!@#$%&*()-+?]*').withMessage('A senha deve estar no padrão'),
     check('usuario.user_password').isLength({ min: 8 }).withMessage('Senha deve possuir o mínimo de 8 caracteres!')
-];
+]
 
 UsuarioValidacoes.adicionar = () => [
     check('usuario.user_name').exists().withMessage('Usuário é obrigatorio'),
@@ -26,15 +26,15 @@ UsuarioValidacoes.adicionar = () => [
     check('usuario.user_full_name').isLength({ min: 3 }).withMessage('O nome precisa ter no mínimo 3 caracteres!'),
     check('usuario.user_password').matches('[A-Za-z0-9!@#$%&*()-+?]*').withMessage('A senha deve estar no padrão'),
     check('usuario.user_password').isLength({ min: 8 }).withMessage('Senha deve possuir o mínimo de 8 caracteres!')
-];
+]
 
 UsuarioValidacoes.id = () => [
     check('id').exists().withMessage('Id é obrigatoria'),
     check('id').isInt().withMessage('Deve ser numerico')
-];
+]
 
 UsuarioValidacoes.nome = () => [
     check('id').exists().withMessage('Nome deve ser digitado')
-];
+]
 
-module.exports = UsuarioValidacoes;
+module.exports = UsuarioValidacoes
